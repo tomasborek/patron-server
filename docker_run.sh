1229297c08477d3ed1a16bd4900ab3ef5c49a399
tomasborek <tomasborek3@gmail.com>
2024-03-12 16:30:07 +0100

    fix: sh variables

--- docker_run.sh
+++ docker_run.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+
+export DOCKER_USERNAME=$1
+export DOCKER_PASSWORD=$2
+export DOCKER_REGISTRY=$3
+
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD $DOCKER_REGISTRY
 
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD $DOCKER_REGISTRY
-docker-compose -f patron/docker-compose.yaml pull
+docker-compose -f ./docker-compose.yaml pull
 
-docker-compose -f patron/docker-compose.yaml down --remove-orphans
-docker-compose -f patron/docker-compose.yaml up -d
+docker-compose -f ./docker-compose.yaml down --remove-orphans
+docker-compose -f ./docker-compose.yaml up -d
 docker system prune --volumes -fa
 
