cbf3d4994ead799bb87dde90b88f655916e9e825
tomasborek <tomasborek3@gmail.com>
2024-03-12 18:28:21 +0100

    fix: deployment

--- docker_run.sh
+++ docker_run.sh
@@ -3,13 +3,13 @@
 
 export DOCKER_USERNAME=$1
 export DOCKER_PASSWORD=$2
-export DOCKER_REGISTRY=$3
+export DOCKER_IMAGE=$3
+export DOCKER_REGISTRY=$4
+export COMMIT_SHA=$5
 
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD $DOCKER_REGISTRY
 
-docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD $DOCKER_REGISTRY
-docker pull $DOCKER_REGISTRY
-
+docker pull $DOCKER_IMAGE:$COMMIT_SHA
 docker compose down --remove-orphans
 docker compose up -d
 docker system prune --volumes -fa
