d15e2f36719b420863415a5552cd1a2de039ba77
tomasborek <tomasborek3@gmail.com>
2024-03-12 15:14:20 +0100

    feat: secure copy docker-compose

--- docker_run.sh
+++ docker_run.sh
@@ -3,9 +3,9 @@
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD $DOCKER_REGISTRY
 
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD $DOCKER_REGISTRY
-docker-compose pull
+docker-compose -f patron/docker-compose.yaml pull
 
-docker-compose down --remove-orphans
-docker-compose -d
+docker-compose -f patron/docker-compose.yaml down --remove-orphans
+docker-compose -f patron/docker-compose.yaml up -d
 docker system prune --volumes -fa
 
