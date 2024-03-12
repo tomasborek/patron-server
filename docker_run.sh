5d9feaae0e96cf5fa8fdbfa535b7c1e838e75a17
tomasborek <tomasborek3@gmail.com>
2024-03-12 16:51:24 +0100

    fix: docker-compose

--- docker_run.sh
+++ docker_run.sh
@@ -8,9 +8,9 @@ export DOCKER_REGISTRY=$3
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD $DOCKER_REGISTRY
 
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD $DOCKER_REGISTRY
-docker-compose -f ./docker-compose.yaml pull
+docker pull $DOCKER_REGISTRY
 
-docker-compose -f ./docker-compose.yaml down --remove-orphans
-docker-compose -f ./docker-compose.yaml up -d
+docker compose down --remove-orphans
+docker compose up -d
 docker system prune --volumes -fa
 
